01072804c2d32aee41fc30010027bd4fb28022d9
Hivef <990059850@qq.com>
2020-09-11 21:22:16 +0800

    登录页面，，，页面访问不了

---
+++ demo0911/html/js/tool.js
@@ -0,0 +1,36 @@
+var tool={
+    success: 200,
+    baseUrl: "http://localhost",
+
+    /*request 请求*/
+    request: function (url, type, params, fun) {
+        var _this = this;
+        $.ajax({
+            url: this.baseUrl+url,
+            type: type,
+            dataType: 'json',
+            data: params,
+            error: function (err) {
+                console.log(err);
+                window.alert("未知异常");
+            },
+            success: function (data) {
+                var status = data.status;
+                if (_this.success == status){
+                    fun(data.data);
+                }else {
+                    window.alert(data.msg);
+                    var code = data.code;
+                    if (code === 403){
+                        window.location.href="login.html";
+                    }
+                }
+            }
+            }
+
+        );
+
+    }
+
+
+}
---
+++ demo0911/src/main/java/com/example/demo0911/Demo0911Application.java
@@ -0,0 +1,13 @@
+package com.example.demo0911;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo0911Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo0911Application.class, args);
+    }
+
+}
